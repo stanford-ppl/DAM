b2cb71eb3bfe2a4c1973184094c8c9404c24da87
Arjun Deopujari <arjund7@stanford.edu>
2023-03-12 20:40:38 -0700

    New vector impl

---
+++ vector/vector.go
@@ -0,0 +1,31 @@
+package vector
+
+import (
+	datatypes "github.com/stanford-ppl/DAM/datatypes/base"
+)
+
+// TODO:  Does golang support another generic param to set length of array
+// that way, we can embed a static-sized array in the struct itself without having
+// to call `NewVector`
+type Vector[T datatypes.DAMType] struct {
+
+	data []T
+
+}
+
+func NewVector[T datatypes.DAMType](width int) *Vector[T] {
+	return &Vector[T]{ data : make([]T , width) }
+}
+
+func (v *Vector[T]) Width() int {
+	return len(v.data)
+}
+
+//TODO:  Need to return optional error if index is out of bounds
+func (v *Vector[T]) set(index int , value T) {
+	v.data[index] = value
+}
+
+func (v *Vector[T]) get(index int) T {
+	return v.data[index]
+}
---
+++ vector/vector_test.go
@@ -0,0 +1,40 @@
+package vector
+
+import (
+	"testing"
+	"github.com/stanford-ppl/DAM/datatypes/fixed"	
+)
+
+func TestNewVector(t *testing.T) {
+	v := NewVector[datatypes.FixedPoint](5)
+	if v.Width() != 5 {
+		t.Errorf("Fail: Vector should be of length: %d but is instead of length: %d" , 5 , v.Width())
+	} else {
+		t.Logf("Pass")
+	}
+}
+
+func TestGetAndSet(t *testing.T) {
+
+	v := NewVector[datatypes.FixPointType](5)
+
+	v.set(0 , datatypes.FixPointType{false, 3, 13})
+	v.set(1 , datatypes.FixPointType{true, 1, 21})
+
+	var elem0 datatypes.FixPointType = v.get(0)
+	var elem1 datatypes.FixPointType = v.get(1)
+
+	if elem0.Signed != false || elem0.Integer != 3 || elem0.Fraction != 13 {
+		t.Errorf("Fail:  Vector Element at index 0 != Element set at index 0")
+	} else {
+		t.Logf("Pass")
+	}
+	
+	if elem1.Signed != true || elem1.Integer != 1 || elem1.Fraction != 21 {
+		t.Errorf("Fail:  Vector Element at index 1 != Element set at index 1")
+	} else {
+		t.Logf("Pass")
+	}
+}
+
+
