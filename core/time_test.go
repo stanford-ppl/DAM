61c05583393c344f2d61e24499cea05f93b5811a
Paul Mure <paulmure2000@gmail.com>
2023-05-29 21:09:50 -0700

    format logging

--- core/time_test.go
+++ core/time_test.go
@@ -8,19 +8,19 @@ import (
 func TestSimpleTime(t *testing.T) {
 	timeZero := NewTime(0)
 	if timeZero.time.Cmp(big.NewInt(0)) != 0 {
-		t.Errorf("Expected: %d, received: %d", 0, timeZero.time.Int64())
+		t.Errorf("Expected: %d, received: %d\n", 0, timeZero.time.Int64())
 	}
 	if timeZero.done {
-		t.Errorf("Expected: not done, received: done")
+		t.Errorf("Expected: not done, received: done\n")
 	}
 
 	timeOne := timeZero.Add(timeZero, NewTime(1))
 	if timeOne.time.Cmp(big.NewInt(1)) != 0 {
-		t.Errorf("Expected: %d, received: %d", 1, timeZero.time.Int64())
+		t.Errorf("Expected: %d, received: %d\n", 1, timeZero.time.Int64())
 	}
 
 	timeTwo := timeOne.Add(timeOne, NewTime(1))
 	if timeTwo.time.Cmp(big.NewInt(2)) != 0 {
-		t.Errorf("Expected: %d, received: %d", 2, timeZero.time.Int64())
+		t.Errorf("Expected: %d, received: %d\n", 2, timeZero.time.Int64())
 	}
 }
